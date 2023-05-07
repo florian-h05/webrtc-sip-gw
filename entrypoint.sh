e1f6d10e4f1dd1a561d7d4d31b34cdea8de0e061
Florian Hotze <florianh_dev@icloud.com>
2023-05-07 22:30:00 +0200

    Fix sed command

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ sed -i -e "s/FILL_MY_DOMAIN/${MY_DOMAIN}/g" /etc/kamailio/kamctlrc
 
 # Allow disabling TLS by setting the TLS_DISABLE env variable to true
 if [ "$TLS_DISABLE" == true ]; then
-  sed -i -e "s/#!define WITH_TLS/##!define WITH_TLS" /etc/kamailio/kamailio.cfg
+  sed -i -e "s/#!define WITH_TLS/##!define WITH_TLS/" /etc/kamailio/kamailio.cfg
 fi
 
 # shellcheck disable=SC2068
