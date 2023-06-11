fdd29364a85052b22c542e6280852ab69d4e9e4d
Florian Hotze <florianh_dev@icloud.com>
2023-06-11 18:41:48 +0200

    Allow manually setting ip & hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-#MY_IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
-MY_IP=$(hostname -I | awk '{print $1}')
-MY_DOMAIN=$(hostname)
+MY_IP=${MY_IP:=$(hostname -I | awk '{print $1}')}
+MY_DOMAIN=${MY_DOMAIN:=$(hostname)}
 
 sed -i -e "s/FILL_MY_IP/${MY_IP}/g" /etc/rtpengine/rtpengine.conf
 sed -i -e "s/FILL_MY_IP/${MY_IP}/g" /etc/kamailio/kamailio.cfg
