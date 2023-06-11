68f77850b046fb016d9b2eb00a84ab27f6a4f294
Florian Hotze <florianh_dev@icloud.com>
2023-06-12 00:23:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 MY_IP=${MY_IP:=$(hostname -I | awk '{print $1}')}
 MY_DOMAIN=${MY_DOMAIN:=$(hostname)}
-SIP_DOMAIN=${SIP_DOMAIN:=fritz.box}
 
 sed -i -e "s/FILL_MY_IP/${MY_IP}/g" /etc/rtpengine/rtpengine.conf
 sed -i -e "s/FILL_MY_IP/${MY_IP}/g" /etc/kamailio/kamailio.cfg
@@ -11,8 +10,6 @@ sed -i -e "s/FILL_MY_IP/${MY_IP}/g" /healthcheck.sh
 
 sed -i -e "s/FILL_MY_DOMAIN/${MY_DOMAIN}/g" /etc/kamailio/kamailio.cfg
 sed -i -e "s/FILL_MY_DOMAIN/${MY_DOMAIN}/g" /etc/kamailio/kamctlrc
-
-sed -i -e "s/FILL_SIP_DOMAIN/${SIP_DOMAIN}/g" /etc/kamailio/kamctlrc
 
 # Allow disabling TLS by setting the TLS_DISABLE env variable to true
 if [ "$TLS_DISABLE" == true ]; then
